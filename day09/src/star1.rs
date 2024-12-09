e6bffd9671f73fd92c6ed4e6017adf41cf30a4cb
gustavLindqvist <gustav.lindqvist000@gmail.com>
2024-12-09 10:41:46 +0100

    day09

--- day09/src/star1.rs
+++ day09/src/star1.rs
@@ -1,4 +1,33 @@
 pub fn star1() {
-    let _ = include_str!("data.in");
-    println!();
+    let data: Vec<_> = include_str!("data.in")
+        .bytes()
+        .map(|u| (u - b'0') as usize)
+        .collect();
+    let mut disc: Vec<usize> = vec![];
+    let mut expteced = 0;
+    for index in (0..data.len() - 1).step_by(2) {
+        expteced += data[index];
+        disc.append(vec![index / 2; data[index]].as_mut());
+        disc.append(vec![usize::MAX; data[index + 1]].as_mut());
+    }
+    disc.append(vec![(data.len() - 1) / 2; data[data.len() - 1]].as_mut());
+    expteced += data[data.len() - 1];
+
+    let mut res = 0;
+    let mut back = disc.len() - 1;
+    for front in 0..disc.len() {
+        if front >= expteced {
+            break;
+        }
+        if disc[front] != usize::MAX {
+            res += disc[front] * front;
+        } else {
+            while disc[back] == usize::MAX {
+                back -= 1;
+            }
+            res += disc[back] * front;
+            back -= 1;
+        }
+    }
+    println!("{}", res);
 }
--- day09/src/star2.rs
+++ day09/src/star2.rs
@@ -1,4 +1,50 @@
 pub fn star2() {
-    let _ = include_str!("data.in");
-    println!();
+    let data: Vec<_> = include_str!("data.in")
+        .bytes()
+        .map(|u| (u - b'0') as usize)
+        .collect();
+    let mut disc: Vec<(usize, usize)> = data
+        .windows(2)
+        .step_by(2)
+        .enumerate()
+        .flat_map(|(i, v)| [(i, v[0]), (usize::MAX, v[1])])
+        .collect();
+    disc.push((data.len() / 2, data[data.len() - 1]));
+
+    'outer: for index in (0..disc.len()).rev() {
+        if disc[index].0 != usize::MAX {
+            for free in 0..disc.len() {
+                if index <= free {
+                    continue;
+                }
+                let sb = disc[index].1;
+                let sf = disc[free].1;
+                if (disc[free].0 == usize::MAX) && (sf >= sb) {
+                    if sf == sb {
+                        disc.swap(index, free);
+                    } else {
+                        let sd = sf - sb;
+                        let tmp = (usize::MAX, sd);
+                        disc[free] = disc[index];
+                        disc[index] = (usize::MAX, sb);
+                        disc.insert(free + 1, tmp);
+                    }
+                    continue 'outer;
+                }
+            }
+        }
+    }
+    let mut bit_disc: Vec<usize> = vec![];
+    for (v, t) in disc {
+        if v != usize::MAX {
+            bit_disc.append(vec![v; t].as_mut());
+        } else {
+            bit_disc.append(vec![0; t].as_mut());
+        }
+    }
+    let res = bit_disc
+        .iter()
+        .enumerate()
+        .fold(0, |sum, (i, v)| sum + i * v);
+    println!("{}", res);
 }
