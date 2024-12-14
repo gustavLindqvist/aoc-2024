a676354b872824ef5420a5d9ca99c9ed14808549
gustavLindqvist <gustav.lindqvist000@gmail.com>
2024-12-15 00:11:11 +0100

    day14

--- day14/src/star1.rs
+++ day14/src/star1.rs
@@ -1,4 +1,39 @@
 pub fn star1() {
-    let _ = include_str!("data.in");
-    println!();
+    let n = 101;
+    let m = 103;
+    let t = 100;
+    let re = regex::Regex::new(r"[^\-0-9]+").unwrap();
+    let robots: Vec<_> = include_str!("data.in")
+        .lines()
+        .map(|l| {
+            re.split(l)
+                .skip(1)
+                .map(|w| w.parse::<i64>().unwrap())
+                .collect::<Vec<_>>()
+        })
+        .map(|v| ((v[0], v[1]), (v[2], v[3])))
+        .collect();
+    let mut res = [0; 4];
+
+    for ((x,y), (dx,dy)) in robots {
+        let x = (x + t * dx.rem_euclid(n)) % n;
+        let y = (y + t * dy.rem_euclid(m)) % m;
+        if x < n/2 {
+            if y < m/2 {
+                res[0] += 1;
+            }
+            if y > m/2 {
+                res[1] += 1;
+            }
+        }
+        if x > n/2 {
+            if y < m/2 {
+                res[2] += 1;
+            }
+            if y > m/2 {
+                res[3] += 1;
+            }
+        }
+    }
+    println!("{}", res.iter().product::<usize>());
 }
--- day14/src/star2.rs
+++ day14/src/star2.rs
@@ -1,4 +1,40 @@
 pub fn star2() {
-    let _ = include_str!("data.in");
-    println!();
+    let n = 101;
+    let m = 103;
+    let re = regex::Regex::new(r"[^\-0-9]+").unwrap();
+    let robots: Vec<_> = include_str!("data.in")
+        .lines()
+        .map(|l| {
+            re.split(l)
+                .skip(1)
+                .map(|w| w.parse::<i64>().unwrap())
+                .collect::<Vec<_>>()
+        })
+        .map(|v| ((v[0], v[1]), (v[2], v[3])))
+        .collect();
+    // Hella ugly men jag är för bakis
+    'outer: for t in 0..n * m {
+        let mut v = std::collections::HashSet::new();
+        for ((x, y), (dx, dy)) in robots.clone() {
+            let x = (x + t * dx.rem_euclid(n)) % n;
+            let y = (y + t * dy.rem_euclid(m)) % m;
+            if v.contains(&(y * n + x)) {
+                continue 'outer;
+            } else {
+                v.insert(y * n + x);
+            }
+        }
+        println!("{}", t);
+        return;
+        // for y in 0..m {
+        //     for x in 0..n {
+        //         if v.contains(&(y, x)) {
+        //             print!("#");
+        //         } else {
+        //             print!(".")
+        //         }
+        //     }
+        //     println!();
+        // }
+    }
 }
