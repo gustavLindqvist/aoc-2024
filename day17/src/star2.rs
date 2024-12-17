09e16d02b4d97da5ca37adaf41cf353947bfef8b
gustavLindqvist <gustav.lindqvist000@gmail.com>
2024-12-17 15:58:36 +0100

    day17

--- day17/src/star2.rs
+++ day17/src/star2.rs
@@ -1,36 +1,19 @@
 pub fn star2() {
     let re = regex::Regex::new(r"[^0-9]+").unwrap();
-    let input = re.split(include_str!("data.in")).skip(4);
+    let input = re.split(include_str!("test1.in")).skip(4);
     let program = input
         .map(|w| w.parse::<usize>().unwrap())
         .collect::<Vec<_>>();
-
-    // Collapsed the loop but only works for my program so cheating
-    // let mut a = 0;
-    // let mut goal = vec![];
-    // 'outer: for b in program.iter().rev() {
-    //     goal.insert(0, *b);
-    //     for test in 0..16 {
-    //         let mut tmp = a + test;
-    //         let mut out = vec![];
-    //         while tmp != 0{
-    //             out.push(((((tmp % 8) ^ 1) ^ 4) ^ (tmp >> ((tmp % 8) ^ 1))) % 8);
-    //             tmp >>= 3;
-    //         }
-    //         if out == goal{
-    //             a += test;
-    //             a <<= 3;
-    //             continue 'outer;
-    //         }
-    //     }
-    // }
-    // a >>= 3;
-
-    let mut a = 0;
-    let mut goal = vec![];
-    'outer: for b in program.iter().rev() {
-        goal.insert(0, *b);
-        for test in 0..16 {
+    let n = program.len();
+    let mut q: Vec<(usize, usize)> = vec![(0, 0)];
+    let mut sol = vec![];
+    while let Some((a, b)) = q.pop() {
+        if b >= program.len() {
+            sol.push(a >> 3);
+            continue;
+        }
+        let goal = &program[(n - b - 1)..n];
+        for test in 0..(1 << 3) {
             let mut registers = [0, 1, 2, 3, a + test, 0, 0];
             let mut out = vec![];
             let mut inst = 0;
@@ -49,20 +32,14 @@ pub fn star2() {
                     5 => out.push(registers[program[inst + 1]] % 8),
                     6 => registers[5] = registers[4] >> registers[program[inst + 1]],
                     7 => registers[6] = registers[4] >> registers[program[inst + 1]],
-                    _ => break,
+                    _ => unreachable!(),
                 }
                 inst += 2;
             }
             if out == goal {
-                println!("{}", test);
-                a += test;
-                a <<= 3;
-                continue 'outer;
+                q.push(((a | test) << 3, b + 1));
             }
         }
-        break;
     }
-    a >>= 3;
-
-    println!("{}", a);
+    println!("{}",sol.iter().min().unwrap());
 }
