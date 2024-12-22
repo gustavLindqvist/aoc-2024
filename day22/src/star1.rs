0741ddee1549bf7e3f6b1e86c31d82d131a80bb7
gustavLindqvist <gustav.lindqvist000@gmail.com>
2024-12-22 14:36:56 +0100

    day22

--- day22/src/star1.rs
+++ day22/src/star1.rs
@@ -1,4 +1,24 @@
 pub fn star1() {
-    let _ = include_str!("data.in");
-    println!();
+    println!(
+        "{}",
+        (0..2000)
+            .fold(
+                include_str!("data.in")
+                    .lines()
+                    .map(|l| l.parse().unwrap())
+                    .collect::<Vec<usize>>(),
+                |acc, _| acc.into_iter().map(morph).collect(),
+            )
+            .iter()
+            .sum::<usize>()
+    );
+}
+fn morph(mut secret: usize) -> usize {
+    secret ^= secret << 6;
+    secret &= (1 << 24) - 1;
+    secret ^= secret >> 5;
+    secret &= (1 << 24) - 1;
+    secret ^= secret << 11;
+    secret &= (1 << 24) - 1;
+    secret
 }
--- day22/src/star2.rs
+++ day22/src/star2.rs
@@ -1,4 +1,46 @@
+use std::collections::{HashMap, HashSet, VecDeque};
+
 pub fn star2() {
-    let _ = include_str!("data.in");
-    println!();
+    let secrets: Vec<_> = include_str!("data.in")
+        .lines()
+        .map(|l| l.parse::<isize>().unwrap())
+        .collect();
+    let mut big_map: HashMap<isize, isize> = HashMap::new();
+    for mut secret in secrets {
+        let mut price = secret % 10;
+        let mut sequence = VecDeque::new();
+        let mut seen = HashSet::new();
+
+        for _ in 0..2_000 {
+            let next_secret = morph(secret);
+            let next_price = next_secret % 10;
+            sequence.push_back(next_price - price + 9);
+            secret = next_secret;
+            price = next_price;
+
+            if sequence.len() > 4 {
+                sequence.pop_front();
+                let hash = hash(&sequence);
+                if !seen.contains(&hash) {
+                    *big_map.entry(hash).or_default() += price;
+                    seen.insert(hash);
+                }
+            }
+        }
+    }
+    println!("{}", big_map.iter().max_by(|a, b| a.1.cmp(b.1)).unwrap().1);
+}
+
+fn hash(seq: &VecDeque<isize>) -> isize {
+    (seq[0] << 12) + (seq[1] << 8) + (seq[2] << 4) + (seq[3])
+}
+
+fn morph(mut secret: isize) -> isize {
+    secret ^= secret << 6;
+    secret &= (1 << 24) - 1;
+    secret ^= secret >> 5;
+    secret &= (1 << 24) - 1;
+    secret ^= secret << 11;
+    secret &= (1 << 24) - 1;
+    secret
 }
