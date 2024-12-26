5fe200f7cd00bf97558faf83e31a0fbc06945cc8
gustavLindqvist <gustav.lindqvist000@gmail.com>
2024-12-26 01:22:16 +0100

    day24

--- day24/src/star1.rs
+++ day24/src/star1.rs
@@ -1,4 +1,36 @@
+use std::collections::{HashMap, VecDeque};
+
 pub fn star1() {
-    let _ = include_str!("data.in");
-    println!();
+    let (start, gates) = include_str!("data.in").split_once("\n\n").unwrap();
+    let mut q = gates
+        .lines()
+        .map(|l| l.split(" ").collect::<Vec<_>>())
+        .map(|l| (l[0], l[1], l[2], l[4]))
+        .collect::<VecDeque<_>>();
+    let mut done = HashMap::new();
+    for (var, val) in start.lines().map(|l| l.split_once(" ").unwrap()) {
+        done.insert(&var[0..=2], val == "1");
+    }
+    while let Some((in1, gate, in2, out)) = q.pop_front() {
+        if done.contains_key(in1) && done.contains_key(in2) {
+            let (a, b) = (done.get(in1).unwrap(), done.get(in2).unwrap());
+            let val = match gate {
+                "XOR" => a ^ b,
+                "AND" => a & b,
+                "OR" => a | b,
+                _ => unreachable!(),
+            };
+            done.insert(out, val);
+        } else {
+            q.push_back((in1, gate, in2, out));
+        }
+    }
+    let mut res: Vec<_> = done
+        .into_iter()
+        .filter(|(k, _)| k.starts_with('z'))
+        .map(|(k, v)| (k, if v { 1 } else { 0 }))
+        .collect();
+    res.sort_by(|a, b| b.cmp(a));
+    let res: usize = res.into_iter().fold(0, |acc, (_, b)| acc * 2 + b);
+    println!("{}", res);
 }
--- day24/src/star2.rs
+++ day24/src/star2.rs
@@ -1,4 +1,51 @@
+use std::collections::{HashSet, VecDeque};
+
 pub fn star2() {
-    let _ = include_str!("data.in");
-    println!();
+    let (_, gates) = include_str!("data.in").split_once("\n\n").unwrap();
+    let q = gates
+        .lines()
+        .map(|l| l.split(" ").collect::<Vec<_>>())
+        .map(|l| (l[0], l[1], l[2], l[4]))
+        .collect::<VecDeque<_>>();
+
+    // Graph with node for all gates
+    let edges: HashSet<_> = q
+        .clone()
+        .into_iter()
+        .flat_map(|(i1, op, i2, _)| [(i1, op), (i2, op)])
+        .collect();
+
+    let mut faulty = vec![];
+
+    for (i1, op, i2, out) in q.clone() {
+        match (i1, i2, op, out) {
+            (i1, i2, "AND", out) if i1 != "x00" && i2 != "x00" && !edges.contains(&(out, "OR")) => {
+                faulty.push(out);
+            }
+            (_, _, "OR", out) if out.starts_with('z') && out != "z45" => {
+                faulty.push(out);
+            }
+            (_, _, "OR", out) if edges.contains(&(out, "OR")) => {
+                faulty.push(out);
+            }
+            (i1, i2, "XOR", out)
+                if (i1.starts_with('x') || i2.starts_with('x'))
+                    && i1 != "x00"
+                    && i2 != "x00"
+                    && !edges.contains(&(out, "XOR")) =>
+            {
+                faulty.push(out);
+            }
+            (_, _, "XOR", out)
+                if !i1.starts_with('x') && !i2.starts_with('x') && !out.starts_with('z') =>
+            {
+                faulty.push(out);
+            }
+            _ => {}
+        }
+    }
+
+    let mut res: Vec<_> = faulty.into_iter().collect();
+    res.sort();
+    println!("{}", res.join(","));
 }
